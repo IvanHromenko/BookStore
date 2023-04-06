c902c2f6d74fadba553df5c34097df34a0fe221e
IvanHromenko <ivangromenko.1003@gmail.com>
2023-04-06 21:28:34 +0300

    added scaffold identity, fixed dbcontext a little

--- BookStore.DataAccess/Data/ApplicationDbContext.cs
+++ BookStore.DataAccess/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
 ﻿
 using BookStore.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookStore.DataAccess
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
@@ -13,5 +15,9 @@ namespace BookStore.DataAccess
         public DbSet<Category> Categories { get; set; }
         public DbSet<Cover> Covers { get; set; }
         public DbSet<Product> Products { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
 }
