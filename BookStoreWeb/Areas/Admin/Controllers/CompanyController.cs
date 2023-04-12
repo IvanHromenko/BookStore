2e3e64184f9183c0a028bf9bf1501918294a123f
IvanHromenko <ivangromenko.1003@gmail.com>
2023-04-12 22:42:37 +0300

    added company controller & view

---
+++ BookStoreWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,96 @@
+﻿using BookStore.DataAccess;
+using BookStore.DataAccess.Repository.IRepository;
+using BookStore.Models;
+using BookStore.Models.ViewModels;
+using BookStore.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Data;
+
+namespace BookStoreWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        
+
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Company> objCompanyList = _unitOfWork.Company.GetAll();
+            return View(objCompanyList);
+        }
+
+        //GET
+        public IActionResult Upsert(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                //create
+                return View(new Company());
+            }
+            else
+            {
+                Company companyObj = _unitOfWork.Company.GetFirstOrDefault(u => u.Id == id);
+                return View(companyObj);
+                //update
+            }
+        }
+
+        //POST
+        [HttpPost]
+        public IActionResult Upsert(Company companyObj)
+        {
+            if (ModelState.IsValid)
+            {
+                if(companyObj.Id == 0)
+                {
+                    _unitOfWork.Company.Add(companyObj);
+                }
+                else
+                {
+                    _unitOfWork.Company.Update(companyObj);
+                }
+                
+                _unitOfWork.Save();
+                TempData["success"] = "Company created successfully";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(companyObj);
+            }
+            
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Company> objCompanyList = _unitOfWork.Company.GetAll().ToList();
+            return Json(new {data = objCompanyList });
+        }
+
+        //POST
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var obj = _unitOfWork.Company.GetFirstOrDefault(u => u.Id == id);
+            if (obj == null)
+            {
+                return Json(new {success = false, message = "Error while deletion"});
+            }
+
+            _unitOfWork.Company.Remove(obj);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete successfull" });
+        }
+        #endregion
+    }
+}
