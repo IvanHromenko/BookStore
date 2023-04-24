ede6347bb6357c1d373c4ffdda29ae65b6f8b153
IvanHromenko <ivangromenko.1003@gmail.com>
2023-04-24 22:32:02 +0300

    order shipping, cancel+refund, payment delayed

--- BookStoreWeb/Areas/Customer/Controllers/CartController.cs
+++ BookStoreWeb/Areas/Customer/Controllers/CartController.cs
@@ -163,7 +163,7 @@ namespace BookStoreWeb.Areas.Customer.Controllers
 
                 if(session.PaymentStatus.ToLower() == "paid")
                 {
-                    _unitOfWork.OrderHeader.UpdateStripePaymentID(ShoppingCartVM.OrderHeader.Id, session.Id, session.PaymentIntentId);
+                    _unitOfWork.OrderHeader.UpdateStripePaymentID(id, session.Id, session.PaymentIntentId);
                     _unitOfWork.OrderHeader.UpdateStatus(id, SD.StatusApproved, SD.PaymentStatusApproved);
                     _unitOfWork.Save();
                 }
